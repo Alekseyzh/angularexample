a7b5f3690a430d4a951236efe01eefa49c794fde
alekseyzh <allbeehappy@gmail.com>
2015-08-22 22:37:59 +0800

    Add payment variant and contacts (raw: no css style)

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -36,4 +36,46 @@
       });
     }
   }
+
 })();
+
+angular.module('myapp').controller('CarouselBeatsCtrl', function ($scope) {
+  $scope.myInterval = 5000;
+  $scope.noWrapSlides = false;
+    $scope.slides = [
+    {
+    src: 'slide1.png',
+    desc: 'Наслаждайся музыкой<br/><span>по-настоящему</span> в наших наушниках <br/> <span>BEATS BY DR.DRE!</span>'
+    },
+    {
+      src: 'slide2.jpg',
+    desc: 'pic2'
+    },
+    {
+      src: 'slide3.png',
+    desc: 'pic3'
+    }
+    ];
+
+});
+
+angular.module('myapp').controller('TabsBeatsCtrl', function ($scope, $window) {
+ 
+});
+
+angular.module('myapp').controller("myCtrl", function($scope, $timeout) {
+    $scope.mapOption = {
+         center: {
+             latitude: 45.4,
+             longitude: -71.9
+         },
+         zoom: 11
+    };
+    $scope.visible = false;
+    $scope.mapViewPosition = {};
+    $scope.$watch("visible", function(newvalue) {
+        $timeout(function() {
+             var map = $scope.myGoogleMap.refresh();
+        }, 0);
+    });
+});
